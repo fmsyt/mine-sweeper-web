dc1d16cafb79d4a58070dd7cb65958113c01cbb1
motsuni (Gemini) <motsuni@yotta-rc.dev>
2026-01-23 15:13:37 +0900

    output: serverを削除して静的サイト生成に戻す

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   site: "https://minesweeper.yotta-rc.dev",
   integrations: [react()],
-  output: "server",
 
   vite: {
     plugins: [
